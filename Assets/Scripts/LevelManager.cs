0919cd131c3ec4d33cd0905dd9338acb0aba540b
Andy Cook <andrewcook1990@gmail.com>
2016-02-21 17:11:18 +0000

    Initial menu systems and placeholder scenes for level flow

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float loadNextDelay = 2.5f;
+
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Invoke("LoadNextLevel", loadNextDelay);
+        }
+    }
+
+
+	public void QuitRequest(){
+		Application.Quit ();
+	}
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] clipsForLevels;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (audioSource && clipsForLevels[level])
+        {
+            // Only play new music if we're not already playing it.
+            if (audioSource.clip != clipsForLevels[level])
+            {
+                audioSource.Stop();
+                audioSource.clip = clipsForLevels[level];
+                audioSource.Play();
+            }
+        }
+    }
+
+}
